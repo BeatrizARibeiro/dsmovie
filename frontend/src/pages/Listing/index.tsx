17a1fe7ce4f5857f6f4d6fb8077cca0146d8f6e2
Beatriz Ribeiro <biaalves.bar@gmail.com>
2022-03-12 13:43:47 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,13 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
-
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
+//FORMA ERRADA
+axios.get(`${BASE_URL}}http://localhost:8080/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data)
+    })
 
 function Listing() {
     return (
